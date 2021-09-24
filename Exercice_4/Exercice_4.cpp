95e210d3f77fda56cdff87201f26151557c72ee7
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2021-09-24 20:06:30 +0200

    Update Exercice4

--- Exercice_4/Exercice_4.cpp
+++ Exercice_4/Exercice_4.cpp
@@ -6,17 +6,17 @@
 
 int main()
 {
-	char age, sexe, preferance, sport, quelsport;
+	int age, sexe, preferance, sport, quelsport;
 	int nbrcinema, nbrpreferance;
 	
 
 	printf("Quel age avez vous ?");
 	scanf("%c", &age);
-	printf("Quel est votre sexe ?");
+	printf("Quel est votre sexe  1 : homme  ou 2 : femme ?");
 	scanf("%c", &sexe);
 	printf("Combien de fois allez vous au cinema par mois");
 	scanf("%d", &nbrcinema);
-	printf("Préférez vous Quick ou Mac_Do ?");
+	printf("Préférez vous  1 :Quick ou 2: Mac_Do ?");
 	
 	if (preferance = Quick || preferance = Mac_Do)
 	{
@@ -24,12 +24,48 @@ int main()
 	}
 	printf(" Combien de fois vous y allez ?");
 	scanf("%d", nbrpreferance);
-	printf("Quel sport pratiquez vous parmi : foot, tennis, basket,judo, natation ?");
-	if (sport = foot|| sport = tennis  || sport = basket || sport = judo || sport = natation);
+	printf("Quel sport pratiquez vous parmi :  1 :foot, 2 : tennis,  3 : basket, 4 : judo, 5 : natation ?");
+	scanf("%c", sport);
+	printf(" Votre age est : %c\n", age);
+	if (sexe == 1);
 	{
-		scanf("%c", sport);
+		printf("votre sexe est homme\n", sexe);
 	}
-	printf(" Votre age est : %c\n, Votre sexe est : %c\n, Le nombre de fois que vous allez au cinéma est : %d\n, Votre préférence est : %c\n, Le nombre de fois que vous y allez : %d\n, Votre sport est : %c", age, sexe, nbrcinema, preferance, nbrpreferance, sport);
+	else if (sexe == 2)
+	{
+		printf("votre sexe est femme\n", sexe);
+	}
+	printf("   Le nombre de fois que vous allez au cinéma est : %d\n", nbrcinema);
+	if (preferance == 1)
+	{
+		printf("votre preferance est Quick.\n");
+	}
+	else if (preferance == 2)
+	{
+		printf("votre preferance est Macdo\n");
+	}
+	printf(" Le nombre de fois que vous y allez : %d\n",nbrpreferance);
+	if ( sport == 1)
+	{
+		printf("Votre sport est foot");
+	}
+	else if ( sport == 2)
+	{
+		printf("Votre sport est tennis");
+	}
+	else if ( sport == 3)
+	{
+		printf("Votre sport est basket");
+	}
+	else if ( sport == 4)
+	{
+		printf("Votre sport est judo");
+	}
+	else if ( sport == 5)
+	{
+		printf("Votre sport est natation");
+	}
+	
 
 
 }
