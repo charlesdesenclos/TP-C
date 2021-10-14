34f43e0607228adf70225894d0f16c3227970fc7
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2021-10-14 14:18:45 +0200

    Add files via upload

--- Chapitre 2/TP3/TP3/TP3.cpp
+++ Chapitre 2/TP3/TP3/TP3.cpp
@@ -1,5 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define gets
 #include <iostream>
 #include<stdio.h>
 #include <conio.h>
@@ -9,15 +8,14 @@
 
 int main()
 {
-	
 	char ch[20], rev[20];
-	int i, j, t=0;
+	int i, j, t;
 	printf("Entrez la chaine");
-	gets( ch );
+	gets(ch);
 	j = 0;
 	
 
-	rev[t] = '\0'; 
+	rev[t] = '\0'; //le dernier caractère doit toujours être égale à '\0'.
 	for (i = t - 1; i >= 0; i--)
 	{
 		rev[j++] = ch[i];
